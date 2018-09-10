98efde831453a67ae0dafe4840c6ae092831326b
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2018-09-10 16:10:39 +0430

    rename gui.py

--- gopem/gui.py
+++
@@ -1,9 +0,0 @@
-from mainwindow import *
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    a = MainWindow()
-    a.show()
-    if app is not None:
-        sys.exit(app.exec_())
