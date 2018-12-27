1b6bb7c21dac73d2d4767d5ec0153df4c18d8634
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2018-12-27 23:59:28 +0330

    Update __main__.py

--- gopem/__main__.py
+++ gopem/__main__.py
@@ -2,10 +2,15 @@ from .mainwindow import *
 import sys
 
 if __name__ == "__main__":
+    print('1')
     app = QApplication(sys.argv)
+    print('2')
     a = MainWindow()
+    print('3')
     if len(sys.argv) > 0 and sys.argv[0] == "--test" and app is not None:
-        sys.exit(0)
+        print('4')
+        sys.exit(app.exec_())
+    print('5')
     a.show()
     if app is not None:
         sys.exit(app.exec_())
