01fb988ca599457990318b077ce005398490e22d
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2018-12-27 18:23:10 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Chemistry',
