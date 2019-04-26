2c70a6941767a09784da30ac3d24ceec866aa463
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2019-04-27 01:02:18 +0430

    docstringing Plotter is Done

--- gopem/plotter.py
+++ gopem/plotter.py
@@ -7,7 +7,7 @@ from matplotlib.figure import Figure
 matplotlib.use('Qt5Agg')  # Make sure that we are using QT5
 
 
-class MyMplCanvas(FigureCanvas):
+class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         """
         The Constructor of MatPlotLib Canvas for plotter
@@ -49,18 +49,39 @@ class MyMplCanvas(FigureCanvas):
 
 class ApplicationWindow(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
+        """
+        The Application Widget for MPLCanvas
+        :param args: The list of arguments
+        :param kwargs: The dictionary of ketwords
+        """
         super().__init__(*args, **kwargs)
         self.setMinimumSize(400, 400)
         l = QtWidgets.QVBoxLayout(self)
-        self.sc = MyMplCanvas(self, width=20, height=20, dpi=100)
+        self.sc = MplCanvas(self, width=20, height=20, dpi=100)
 
         l.addWidget(self.sc)
 
     def update_plotter_data(self, data, x_axis, y_axis):
+        """
+        Update the plotter data and axis
+        :param data: The dictionary of data
+        :param x_axis: The Ticks on X axis
+        :param y_axis:  The Ticks on Y axis
+        :return: None
+        """
         self.sc.update_plot(data, x_axis, y_axis)
 
     def file_quit(self):
+        """
+        Close the application
+        :return:
+        """
         self.close()
 
     def close_event(self, ce):
+        """
+        The Slot for close event trigger
+        :param ce: Colse Event
+        :return: None
+        """
         self.file_quit()
