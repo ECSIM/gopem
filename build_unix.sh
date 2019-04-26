54700ba03631bb76e18d6c9f795b6a793cc757b1
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-04-26 13:12:55 +0430

    fix : minor edit in build_unix.sh

--- build_unix.sh
+++ build_unix.sh
@@ -1,11 +1,11 @@
-#bin/bash
+#!/bin/sh
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        pyinstaller -y --clean --windowed OPEM.spec
+        pyinstaller -y --clean --windowed GOPEM.spec
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-        pyinstaller -y --clean --windowed OPEM.spec
+        pyinstaller -y --clean --windowed GOPEM.spec
 	pushd dist
-	hdiutil create ./Opem.dmg -srcfolder OPEM -ov
+	hdiutil create ./GOpem.dmg -srcfolder GOPEM -ov
 	popd
 elif [[ "$OSTYPE" == "cygwin" ]]; then
         echo "You are usign $OSTYPE, that means you are using windows OS, so please run build_exe.bat instead."
@@ -14,5 +14,5 @@ elif [[ "$OSTYPE" == "msys" ]]; then
 elif [[ "$OSTYPE" == "msys" ]]; then
         echo "You are usign $OSTYPE, that means you are using windows OS, so please run build_exe.bat instead."
 else
-        echo "$OSTYPE is not compatiable. please add an issue for opem."
+        echo "$OSTYPE is not compatiable. please add an issue for gopem."
 fi
