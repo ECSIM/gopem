6246e835f330bd7e61011d930ee8cf76c1f28db7
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-04-27 18:44:30 +0430

    fix : setup.py modified

--- setup.py
+++ setup.py
@@ -23,30 +23,21 @@ def read_description():
             description += c.read()
         return description
     except Exception:
-        return '''
-  Modeling and simulation of proton-exchange membrane fuel cells (PEMFC) may work as a powerful tool in the Research &
-  development of renewable energy sources. The Open-Source PEMFC Simulation Tool (OPEM) is a modeling tool for
-  evaluating the performance of proton exchange membrane fuel cells. This package is a combination of models
-  (static/dynamic) that predict the optimum operating parameters of PEMFC. OPEM contained generic models that
-  will accept as input, not only values of the operating variables such as anode and cathode feed gas, pressure
-  and compositions, cell temperature and current density, but also cell parameters including the active area and
-  membrane thickness. In addition, some of the different models of PEMFC that have been proposed in the OPEM,
-  just focus on one particular FC stack, and some others take into account a part or all auxiliaries such as
-  reformers. OPEM is a platform for collaborative development of PEMFC models.'''
+        return "GOPEM is a graphical user interface of OPEM (Open Source PEM Fuel Cell Simulation Tool)"
 
 
 setup(
     name='gopem',
     packages=['gopem'],
     version='0.2',
-    description='Open Source GUI Application for OPEM',
+    description='GOPEM is a graphical user interface of OPEM',
     long_description=read_description(),
     long_description_content_type='text/markdown',
     author='Mohammad Mahdi Rahimi,Sepand Haghighi,Kasra Askari,Sarmin Hamidi',
     author_email='opem@ecsim.ir',
     url='https://github.com/ecsim/gopem',
     download_url='https://github.com/ecsim/gopem/tarball/v0.2',
-    keywords="OPEM PEM FC CELL Fuel-Cell Chemistry GUI PyQt",
+    keywords="OPEM PEM FC CELL Fuel-Cell Chemistry GUI PyQt GOPEM",
     project_urls={
         'Webpage': 'http://opem.ecsim.ir',
         'Say Thanks!': 'https://saythanks.io/to/ecsim',
@@ -64,6 +55,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
     ],
     license='MIT',
 )
