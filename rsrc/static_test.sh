1bcf6f2c85e1606bb1553ba77d391665461982ea
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-10-04 00:49:53 +0330

    fix : static_test modified

--- rsrc/static_test.sh
+++ rsrc/static_test.sh
@@ -2,7 +2,20 @@
 # Dump Environment (so that we can check PATH, UT_FLAGS, etc.)
  set -e
  set -x
- if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
+ IS_IN_TRAVIS=false
+ PYTHON_COMMAND=python
+  
+ if [ "$TRAVIS_OS_NAME" == "osx" ]
+ then
+	PYTHON_COMMAND=python3
+ fi
+ 
+ if [ "$CI" = 'true' ] && [ "$TRAVIS" = 'true' ]
+ then
+      IS_IN_TRAVIS=true
+ fi
+  
+ if [ "$IS_IN_TRAVIS" = 'false' ] || [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
  then
      python -m vulture gopem/ rsrc/ setup.py --min-confidence 65 --exclude=gopem,build,.eggs --sort-by-size .
 	 python -m bandit -r gopem -s B322
