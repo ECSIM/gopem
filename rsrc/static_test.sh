4ed2753f9234fc9faf2a78eea5b329e678d39848
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-10-04 00:48:13 +0330

    fix : vulture test command fixed

--- rsrc/static_test.sh
+++ rsrc/static_test.sh
@@ -4,7 +4,7 @@
  set -x
  if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
  then
-     python -m vulture --min-confidence 80 --exclude=gopem,build,.eggs --sort-by-size .
+     python -m vulture gopem/ rsrc/ setup.py --min-confidence 65 --exclude=gopem,build,.eggs --sort-by-size .
 	 python -m bandit -r gopem -s B322
 	 python rsrc/version_check.py
 	 python -m pydocstyle
