0f9f7e514b472434d8c1fbf314e2811163f7e442
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-10-14 10:47:45 +0330

    fix : minor edit in FontSizeList range

--- gopem/helper.py
+++ gopem/helper.py
@@ -107,4 +107,4 @@ StyleList.insert(0, StyleList.pop(StyleList.index("Solid")))
 ScaleList = ["Linear", "Log"]
 WidthList = list(range(1, 11))
 
-FontSizeList = list(range(1,55))
+FontSizeList = list(range(1,56))
