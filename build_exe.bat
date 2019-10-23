b6b9064a0735e99f69006191be11ed87dc6acaef
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-10-23 15:57:07 +0330

    fix : build_exe updated

--- build_exe.bat
+++ build_exe.bat
@@ -2,13 +2,11 @@
 FOR /F "tokens=* USEBACKQ" %%F IN (`python --version`) DO (
 SET py_version_str=%%F
 )
-SET /A py_version=%py_version_str:~9,-2%
+SET py_version=%py_version_str:~7%
+
+echo Your Python Version : %py_version%
+echo Recommended Python Version : ^>= 3.5
 echo -----
-if %py_version% LSS 5 (
-	echo Warning : Please use Python ^> 3.5
-) else (
-	echo Python version check done!
-)
 echo -----
 python -m PyInstaller GOPEM.spec
 pause
