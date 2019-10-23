c48e685cdd672eec91b8d8d4d0de7415e4256157
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-10-23 16:10:59 +0330

    fix : build_unix.sh updated

--- build_unix.sh
+++ build_unix.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-
+py_version=$(python3 -V 2>&1 | grep -Po '(?<=Python )(.+)')
+echo "Your Python Version : $py_version"
+echo "Recommended Python Version : >= 3.5"
+echo "-----"
+echo "-----"
+pip3 install -r requirements.txt
+python3 setup.py install
+pip3 install PyInstaller>=3.3
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         pyinstaller -y --clean --windowed GOPEM.spec
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-        pyinstaller -y --clean --windowed GOPEM.spec
+    pyinstaller -y --clean --windowed GOPEM.spec
 	pushd dist
+	rm -r GOPEM
+	mkdir GOPEM
+	mv GOPEM-Portable GOPEM/
 	hdiutil create ./GOPEM.dmg -srcfolder GOPEM -ov
 	popd
 elif [[ "$OSTYPE" == "cygwin" ]]; then
