9ab7886b01e8061228aed5d9b3e2c9c92959d958
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-03-16 19:38:40 +0330

    fix : build scripts updated

--- build_exe.bat
+++ build_exe.bat
@@ -8,12 +8,12 @@ echo Your Python Version : %py_version%
 echo Recommended Python Version : ^>= 3.5
 echo -----
 echo -----
+python -m pip install "art==4.0"
 python -m pip install "requests==2.22.0"
 python -m pip install "matplotlib==3.1.1"
 python -m pip install "opem==1.2"
 python -m pip install "PyQt5==5.13.1"
 python -m pip install "setuptools==40.9.0"
 python -m pip install "PyInstaller==3.4"
-python setup.py install
 python -m PyInstaller GOPEM.spec
 pause
--- build_unix.sh
+++ build_unix.sh
@@ -4,13 +4,13 @@ echo "Your Python Version : $py_version"
 echo "Recommended Python Version : >= 3.5"
 echo "-----"
 echo "-----"
+pip3 install "art==4.0"
 pip3 install "requests==2.22.0"
 pip3 install "matplotlib==3.1.1"
 pip3 install "opem==1.2"
 pip3 install "PyQt5==5.13.1"
 pip3 install "setuptools==40.9.0"
 pip3 install "PyInstaller==3.4"
-python3 setup.py install
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         pyinstaller -y --clean --windowed GOPEM.spec
 elif [[ "$OSTYPE" == "darwin"* ]]; then
