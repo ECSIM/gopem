41f4cc2e4f20e0e67500c0f47ed451c400892c3f
sepandhaghighi <sepand.haghighi@yahoo.com>
2023-12-18 17:46:16 +0330

    fix : autpep8 scripts updated

--- autopep8.bat
+++ autopep8.bat
@@ -1,3 +1,3 @@
-python -m autopep8 gopem --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
-python -m autopep8 setup.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
-python -m autopep8 rsrc --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
+python -m autopep8 gopem --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --max-line-length 120 --verbose
+python -m autopep8 setup.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --max-line-length 120 --verbose
+python -m autopep8 rsrc --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --max-line-length 120 --verbose
