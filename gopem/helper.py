ca1a75a383fad84d7575928d4f4756ab3a8745b4
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-05-17 17:59:48 -0700

    fix : `Linear` --> `linear` in `ScaleList`.

--- gopem/helper.py
+++ gopem/helper.py
@@ -116,7 +116,7 @@ StyleTable = {"Solid": "-", "Dashed": "--", "Dash-Dot": "-.", "Dotted": ":"}
 StyleList = sorted(StyleTable)
 StyleList.insert(0, StyleList.pop(StyleList.index("Solid")))
 
-ScaleList = ["Linear", "Log"]
+ScaleList = ["linear", "Log"]
 WidthList = list(range(1, 11))
 
 FontSizeList = list(range(1, 56))
