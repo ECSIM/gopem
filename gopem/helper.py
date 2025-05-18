a25d5dcbd5512c535f41c2e88c6af2ecb78b6b7a
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-05-17 18:07:41 -0700

    change : `Log` --> `log`.

--- gopem/helper.py
+++ gopem/helper.py
@@ -116,7 +116,7 @@ StyleTable = {"Solid": "-", "Dashed": "--", "Dash-Dot": "-.", "Dotted": ":"}
 StyleList = sorted(StyleTable)
 StyleList.insert(0, StyleList.pop(StyleList.index("Solid")))
 
-ScaleList = ["linear", "Log"]
+ScaleList = ["linear", "log"]
 WidthList = list(range(1, 11))
 
 FontSizeList = list(range(1, 56))
